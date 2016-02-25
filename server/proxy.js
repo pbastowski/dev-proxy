52540d6d8a6d36defc01e6360a854a3c5ff3fa3e
Paul Bastowski <pbastowski@gmail.com>
2016-02-25 14:19:30 +1000

    Added exactMatch property. True means source url must exactly match req.url to activate proxying. Gitignore .idea

--- server/proxy.js
+++ server/proxy.js
@@ -11,7 +11,7 @@
 
         // Did the user pass up to three parameters?
         if ("string" === typeof source && target) {
-            proxies = [{ source: source, target: target, replacePath: replacePath }];
+            proxies = [{ source: source, target: target, replacePath: replacePath, exactMatch: false }];
         }
         // Or perhaps they passed an array of objects?
         else if ( source instanceof Array) {
@@ -23,10 +23,10 @@
         // Enable all requested proxies
         proxies.forEach(function(config) {
             console.log('Proxy config:\n', JSON.stringify(config, null, 4));
-            makeProxy( config.source, config.target, config.replacePath );
+            makeProxy( config.source, config.target, config.replacePath, config.exactMatch || false );
         });
 
-        function makeProxy (source, target, replacePath) {
+        function makeProxy (source, target, replacePath, exactMatch) {
             var apiProxy = httpProxy.createProxyServer({
                 target: target,
                 changeOrigin: !!replacePath
@@ -34,7 +34,7 @@
 
             WebApp.connectHandlers
                 .use(function (req, res, next) {
-                    if (req.url.indexOf(source) > -1) {
+                    if ((!exactMatch && req.url.indexOf(source) > -1) || (exactMatch && req.url === source)) {
                         if (replacePath)
                             req.url = req.url.replace(replacePath.search, replacePath.replace);
 
